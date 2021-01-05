6417508e9c073aa55b0a9c2446c3d8db19ee479e
Lorenzo Rizzotti <10401566+Dreaming-Codes@users.noreply.github.com>
2021-01-05 08:24:34 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+name "MyFivemProject"
+
+description ""
+
+author ""
+
+version "1.0"
+
+url ""
+
+client_script "MyFivemProject_CLIENT.net.dll"
+
+server_script "MyFivemProject_SERVER.net.dll"
+
+fx_version "cerulean"
+game "gta5"
