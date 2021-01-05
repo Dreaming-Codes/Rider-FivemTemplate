483e52eb34ca6fc8b6d749162348edcace4dd477
Lorenzo Rizzotti <10401566+Dreaming-Codes@users.noreply.github.com>
2021-01-05 08:13:55 +0100

    Add files via upload

---
+++ MyFivemProject_SERVER/Server.cs
@@ -0,0 +1,13 @@
+﻿#region Dll di fivem
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+#endregion
+
+namespace MyFivemProject_SERVER {
+    public class Client : BaseScript {
+        public Client()
+        {
+            
+        }
+    }
+}
